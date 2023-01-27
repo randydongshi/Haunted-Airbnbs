ed2c672d8613c8550d4fe350ce8ea82f0cd699ba
randydongshi <116231776+randydongshi@users.noreply.github.com>
2023-01-27 15:46:07 -0500

    Add files via upload

---
+++ Haunted Airbnbs.sql
@@ -0,0 +1,4 @@
+select host_id, host_url, host_name, count(*) as haunted_mentions from chicago_reviews
+inner join chicago_listings on chicago_listings.id = chicago_reviews.listing_id
+where comments like '%fuck%'
+group by host_id, host_url, host_name order by haunted_mentions desc;
